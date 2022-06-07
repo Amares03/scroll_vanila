4fbf5e2a52fdccfe994c0ecb78c6d1442335f310
Amares03 <amaressabit0314@gmail.com>
2022-06-07 15:12:48 +0300

    make nav toggler responsive and dynamic

--- app.js
+++ app.js
@@ -4,9 +4,23 @@
 //offsetTop - A Number, representing the top position of the element, in pixels
 
 // ********** set date ************
+const date = document.getElementById("date");
+date.innerHTML = new Date().getFullYear();
 
 // ********** close links ************
+const navToggle = document.querySelector(".nav-toggle");
+const linksContainer = document.querySelector(".links-container");
+const links = document.querySelector(".links");
 
+navToggle.addEventListener("click", function () {
+  const contaionerHight = linksContainer.getBoundingClientRect().height;
+  const linksHight = links.getBoundingClientRect().height;
+  if (contaionerHight === 0) {
+    linksContainer.style.height = `${linksHight}px`;
+  } else {
+    linksContainer.style.height = 0;
+  }
+});
 // ********** fixed navbar ************
 
 // ********** smooth scroll ************
